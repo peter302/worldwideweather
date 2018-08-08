7d9be1f17d780f5356e0e0e4d24c91968cae0d99
peter302 <petermbaik@gmail.com>
2018-08-08 16:53:41 +0300

    modified url of ajax function

--- worldwideweather.js
+++ worldwideweather.js
@@ -1,42 +1,24 @@
 //front-end logic
-$.(document).ready(function(){
-  $.("#sub2").click(function(event){
+$(document).ready(function(){
+  $("#sub2").click(function(event){
     event.preventDefault();
-    var city=$.("#submit").val()
-    if(city !=''){
+    var city=$("#submit").val()
     $.ajax({
-      url:"http://api.openweathermap.org/data/2.5/weather?q="+city+"$appid=5c3e6d8e67832b9a2a8451929f80bda4"
-      type:"GET"
-      datatype:"jsonp"
+      url:'http://api.openweathermap.org/data/2.5/weather?q='+city+ "&units=metric"+"&APPID=5c3e6d8e67832b9a2a8451929f80bda4",
+      type:"GET",
+      dataType:"jsonp",
       success:function(data){
-        var callback=display(data)
-        $.("#display").html(callback);
-        $.("#submit").val("");
+        var callback=display(data);
+        $("#display").html(callback)
+        $("#submit").val("")
 
       }
     })
-  }
-  else{
-    $.("#error").html(<div class="alert alert-info alert-dismissible">
-    <button type="button" class="close" data-dismiss="alert">&times;</button>
-    <strong>City cannot be empty!</strong>
-  </div>)
-
-  }
-}
-}
+})
+})
 //end front-end logic
 //business logic
 function display(data){
-  return "<h2>weather condition:"+data.weather[0].main+"</h2>"
+  return "<h2>weather condition:"+data.weather[0].main+"</h2>"+
          "<h2>weather description:"+data.weather[0].description+"</h2>"
-         "<h2>wind speed:"+data.wind.speed+"</h2>"
-         "<h2>wind dir:"+data.wind.deg+"</h2>"
-         "<h2>temp:"+data.main.temp+"</h2>"
-         "<h2>min temp:"+data.main.temp_min+"</h2>"
-         "<h2>max temp:"+data.main.temp_max+"</h2>"
-         "<h2>sea level"+ data.main.sea_level+"</h2>"
-         "<h2>ground level:"+data.main.grnd_level+"</h2>"
-         "<h2>humidity:"+data.main.humidity+"</h2>"
-
 }
