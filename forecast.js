cdf22ad977b7019733fcf8969d98c56062bc58b6
peter302 <petermbaik@gmail.com>
2018-08-10 15:33:04 +0300

    added ping pong and pigdice to worwide weather

---
+++ forecast.js
@@ -0,0 +1,40 @@
+function forecast(){
+  navigator.geolocation.getCurrentPosition(location);
+  function location(position){
+    var longitude=position.coords.longitude;
+    var latitude=position.coords.latitude;
+    $("#location").html(longitude +","+ latitude);
+    $.ajax({
+      url:"http://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&units=metric"+"&APPID=5c3e6d8e67832b9a2a8451929f80bda4",
+      type:"GET",
+      dataType:"jsonp",
+      success:function(data){
+          var imp=display(data);
+          $("#anger").html(imp)
+
+
+        }
+
+      })
+    }
+  $("#anger").html("loading.....")
+  }
+forecast();
+function display(data){
+  return "<h2 style='font-size:15px'>current weather for:"+data.name+" "+data.sys.country+"</h2>"+
+         "<h2 style='font-size:15px'>longtude:"+data.coord.lon+"&deg</h2>"+
+         "<h2 style='font-size:15px'>latitude:"+data.coord.lat+"&deg</h2>"+
+         "<h2 style='font-size:15px'>weather condition:"+data.weather[0].main+"</h2>"+
+         "<h2 style='font-size:15px'>weather description:"+data.weather[0].description+"</h2>"+
+         "<h2 style='font-size:15px'>temp:"+data.main.temp+"&deg C</h2>"+
+         "<h2 style='font-size:15px'>temp_min:"+data.main.temp_min+"&deg C</h2>"+
+         "<h2 style='font-size:15px'>temp_max"+data.main.temp_max+"&deg C</h2>"+
+         "<h2 style='font-size:15px'>humidity:"+data.main.humidity+"%</h2>"+
+         "<h2 style='font-size:15px'>pressure:"+data.main.pressure+"hpa</h2>"+
+         "<h2 style='font-size:15px'>sea_level:"+data.main.sea_level+"hpa</h2>"+
+         "<h2 style='font-size:15px'>grnd_level:"+data.main.grnd_level+"hpa</h2>"+
+         "<h2 style='font-size:15px'>wind speed:"+data.wind.speed+"m/s</h2>"+
+         "<h2 style='font-size:15px'>wind direction:"+data.wind.deg+"&deg</h2>"+
+         "<h2 style='font-size:15px'>clouds:"+data.clouds.all+"%</h2>"
+
+}
--- leo.js
+++ leo.js
@@ -21,20 +21,20 @@ function weather(){
   }
 weather();
 function display(data){
-  return "<h2>current weather for:"+data.name+" "+data.sys.country+"</h2>"+
-         "<h2>longtude:"+data.coord.lon+"&deg</h2>"+
-         "<h2>latitude:"+data.coord.lat+"&deg</h2>"+
-         "<h2>weather condition:"+data.weather[0].main+"</h2>"+
-         "<h2>weather description:"+data.weather[0].description+"</h2>"+
-         "<h2>temp:"+data.main.temp+"&deg C</h2>"+
-         "<h2>temp_min:"+data.main.temp_min+"&deg C</h2>"+
-         "<h2>temp_max"+data.main.temp_max+"&deg C</h2>"+
-         "<h2>humidity:"+data.main.humidity+"%</h2>"+
-         "<h2>pressure:"+data.main.pressure+"hpa</h2>"+
-         "<h2>sea_level:"+data.main.sea_level+"hpa</h2>"+
-         "<h2>grnd_level:"+data.main.grnd_level+"hpa</h2>"+
-         "<h2>wind speed:"+data.wind.speed+"m/s</h2>"+
-         "<h2>wind direction:"+data.wind.deg+"&deg</h2>"+
-         "<h2>clouds:"+data.clouds.all+"%</h2>"
+  return "<h2 style='font-size:10px'>current weather for:"+data.name+" "+data.sys.country+"</h2>"+
+         "<h2 style='font-size:10px'>longtude:"+data.coord.lon+"&deg</h2>"+
+         "<h2 style='font-size:10px'>latitude:"+data.coord.lat+"&deg</h2>"+
+         "<h2 style='font-size:10px'>weather condition:"+data.weather[0].main+"</h2>"+
+         "<h2 style='font-size:10px'>weather description:"<img href='http://openweathermap.org/img/w/'+data.weather[0].description+'.png'>+data.weather[0].description+"</h2>"+
+         "<h2 style='font-size:10px'>temp:"+data.main.temp+"&deg C</h2>"+
+         "<h2 style='font-size:10px'>temp_min:"+data.main.temp_min+"&deg C</h2>"+
+         "<h2 style='font-size:10px'>temp_max"+data.main.temp_max+"&deg C</h2>"+
+         "<h2 style='font-size:10px'>humidity:"+data.main.humidity+"%</h2>"+
+         "<h2 style='font-size:10px'>pressure:"+data.main.pressure+"hpa</h2>"+
+         "<h2 style='font-size:10px'>sea_level:"+data.main.sea_level+"hpa</h2>"+
+         "<h2 style='font-size:10px'>grnd_level:"+data.main.grnd_level+"hpa</h2>"+
+         "<h2 style='font-size:10px'>wind speed:"+data.wind.speed+"m/s</h2>"+
+         "<h2 style='font-size:10px'>wind direction:"+data.wind.deg+"&deg</h2>"+
+         "<h2 style='font-size:10px'>clouds:"+data.clouds.all+"%</h2>"
 
 }
